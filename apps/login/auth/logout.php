6ae5de719d60daacbccb5e1877c85d89bf2397d0
RmzGerardo <lalitomendez132@gmail.com>
2026-01-27 17:47:06 -0600

    Cambios en header para poner el nombre

--- apps/login/auth/logout.php
+++ apps/login/auth/logout.php
@@ -1,11 +1,11 @@
 <?php
-setcookie("access_token","",[
-'expires' => time() - 3600,
-'path' => '/',
-'httponly' => true,
-'samesite' => 'Lax'
+setcookie("access_token", "", [
+    'expires' => time() - 3600,
+    'path' => '/',
+    'httponly' => true,
+    'samesite' => 'Lax'
 ]);
 
-header("Location: http://10.10.30.28:8086/");
+header("Location: http://localhost:8086/");
 exit();
 
