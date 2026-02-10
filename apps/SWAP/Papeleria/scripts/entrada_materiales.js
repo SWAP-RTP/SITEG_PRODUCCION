2e61e025febea008f3105d96a559663a17a5100d
Raul Hernandez <raul.hernandez.chaires@estudiante.uacm.edu.mx>
2026-02-09 18:16:30 -0600

    se crea el archivo  js entrada_materiales dentro de la carpeta scripts

---
+++ apps/SWAP/Papeleria/scripts/entrada_materiales.js
@@ -0,0 +1,7 @@
+//se crea el documentoe de manipulación de la entrada de materiales, 
+// se agrega un evento para cargar el js al cargar la página y se agrega un console 
+// log para verificar que se cargue correctamente el js
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Entrada de materiales, prueba de carga de js");
+}
+)
