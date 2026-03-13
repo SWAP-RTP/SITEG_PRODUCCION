199eb69a7a21397aafc736c6a2cf567d2a53edac
Mjhernandez12712 <mauricio.h.javier.o@hotmail.com>
2026-03-13 13:25:17 -0600

    Cambios al corriente

--- apps/login/admin/query_sql/modulos_sistema_update.php
+++ apps/login/admin/query_sql/modulos_sistema_update.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: application/json');
-require_once __DIR__ . '/../../conf/conexion.php';
+require_once "../../conf/conexion.php";
 
 // Recibir datos JSON
 $input = json_decode(file_get_contents('php://input'), true);
