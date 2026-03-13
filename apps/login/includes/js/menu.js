8ef6dbe12802333d5d48a8e58de4b98edb07dcc1
Mjhernandez12712 <mauricio.h.javier.o@hotmail.com>
2026-03-13 14:42:28 -0600

    Correccion de las rutas fijas, ahora es dinamica y no importa si es localhost o el servidor, siempre funcionara y no habra que modificar mas

--- apps/login/includes/js/menu.js
+++ apps/login/includes/js/menu.js
@@ -1,13 +1,12 @@
 function sistemas_acceso() {
-
-  $.ajax({
-      url: "/includes/query_sql/sistemas_sinteg.php",
-      type: "GET",
-      dataType: "json",
-      success: function(data){
-          // console.log(data);
-          data.forEach((sistema) => {
-              let cards_dinamicos = `
+    $.ajax({
+        url: "/includes/query_sql/sistemas_sinteg.php",
+        type: "GET",
+        dataType: "json",
+        success: function (data) {
+            // console.log(data);
+            data.forEach((sistema) => {
+                let cards_dinamicos = `
                   <div class="card fade-card d-flex flex-column align-items-center text-center">
                       <div class="mt-3 mb-3 ms-3 me-3">
                           <img src="../includes/img/${sistema.sistema_imagen}" alt="${sistema.acronimo}">
@@ -20,29 +19,29 @@ function sistemas_acceso() {
                       </div>
                   </div>
               `;
-              $('#container').append(cards_dinamicos);
-          });
+                $('#container').append(cards_dinamicos);
+            });
 
-          // aplicamos un retrazo de aparicion por cada card que dibuja
-          const cards = document.querySelectorAll(".card");
+            // aplicamos un retrazo de aparicion por cada card que dibuja
+            const cards = document.querySelectorAll(".card");
 
-          cards.forEach((card, index) => {
-            card.style.animationDelay = (index * 0.3) + "s";
-          });
-      },
-      error: function(xhr){
-          console.log("Error:", xhr.responseText);
-      }
-  });
+            cards.forEach((card, index) => {
+                card.style.animationDelay = (index * 0.3) + "s";
+            });
+        },
+        error: function (xhr) {
+            console.log("Error:", xhr.responseText);
+        }
+    });
 }
 
 // CLICK DINÁMICO
 $(document).on("click", ".btn-sistema", function () {
-  let sistema = $(this).attr("data-sistema");
-  // esto es igual a http://localhost:8086/app-swap/
-  let url = "http://localhost:8086/" + "app-" + sistema + "/";
+    let sistema = $(this).attr("data-sistema");
+    // esto es igual a http://localhost:8086/app-swap/
+    let url = `${window.location.origin}/app-${sistema}/`;
 
-  window.open(url, "_blank");
+    window.open(url, "_blank");
 });
 
 // CLICK CUANDO EL MENU ES RESPONSIVO
@@ -52,5 +51,5 @@ $(document).on("click", ".hamburger", function () {
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-  sistemas_acceso();
+    sistemas_acceso();
 });
